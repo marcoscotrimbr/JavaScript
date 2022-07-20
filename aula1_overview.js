8f4efe2d221885eeea72f5f003e9bc86281351b7
Marcos Cotrim <94663094+marcoscotrimbr@users.noreply.github.com>
2022-07-19 22:34:11 -0300

    Add files via upload
    USP Data Science - Aula 1 Java Script

---
+++ aula1_overview.js
@@ -0,0 +1,40 @@
+// Comentário de linha
+
+/*
+Comentário de bloco...
+Veja, pode ter quebra de linha.
+*/
+
+// Variáveis
+var nome = "Marcos Cotrim"
+var idade = 09 //variavel tipo int
+var idoso = false   //boolean
+var valor_total = 10.99 // variavel double
+var comidas_prediletas = ["pizza", "japonesa"] // variavel array
+
+
+//console.log ("Olá mundo, meu nome é", nome, "e tenho", idade, "anos") 
+//console.log ("Minha comida predileta é", comidas_prediletas) 
+
+//console.log(nome.length)   // tamanho do texto
+//console.log(nome.toUpperCase()) // DEIXA MAIUSCULO
+//console.log (nome.toLocaleLowerCase()) // deixa minusculo
+
+
+// Operadores
+//console.log(idade > 18) // maior
+//console.log(idade < 18) // menor
+
+
+// Condicional
+if (idade>18){
+   console.log("Maior de idade")
+} else{
+    console.log("Menor de idade, não pode beber")
+}
+
+// Loop
+
+// Funções
+
+// 
